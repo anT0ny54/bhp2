cc11a0311497d8ed940ba70e6bf4aa08c282fca7
anT0ny54 <109664822+anT0ny54@users.noreply.github.com>
2026-09-13 11:08:17 +0700

    Delete util directory

--- util/pick.js
+++
@@ -1,5 +0,0 @@
-export default function pick(object, properties) {
-  const source = object && typeof object === "object" ? object : {};
-  const allowed = new Set(properties || []);
-  return Object.fromEntries(Object.entries(source).filter(([key]) => allowed.has(key)));
-}
--- util/shouldCompress.js
+++
@@ -1,4 +0,0 @@
-const MIN_COMPRESS_LENGTH = 1024;
-export default function shouldCompress(contentType, size) {
-  return typeof contentType === "string" && contentType.toLowerCase().startsWith("image/") && Number.isFinite(size) && size >= MIN_COMPRESS_LENGTH;
-}
